78ee55861434ab022a2acb829a4bca9863913444
sreynik123 <sreyniklout992@gmail.com>
2025-01-21 14:36:17 -0800

    first commit

--- src/page/Home1.jsx
+++ src/page/Home1.jsx
@@ -31,7 +31,7 @@ export default function Home1() {
       </div>
       <hr />
       <ul className=" bg-white p-5   grid gap-x-8 gap-y-10 mt-16 sm:grid-cols-2 lg:grid-cols-3 shadow-2xl">
-        {data.results &&
+        {data.results  &&
           data.results.map((movie) => (
             <li className="w-full mx-auto group sm:max-w-sm hover:text-blue-800" >
               <a href="#">
@@ -45,7 +45,7 @@ export default function Home1() {
                   <span className="block text-indigo-600 text-sm">
                     {movie.release_date}
                   </span>
-                  <h3 className="text-lg text-gray-800 duration-150 group-hover:text-indigo-600 font-semibold">
+                  <h3 className="text-lg text-gray-800 duration-150 group-hover:text-indigo-600  font-semibold">
                     {movie.original_title}
                   </h3>
                   <p className="text-gray-600 text-sm duration-150 group-hover:text-gray-800">
